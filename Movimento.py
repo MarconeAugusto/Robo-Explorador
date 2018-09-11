10a8f41e1e1f135af3d1f90483ea2b4d7e303f53
yanmartins <30724634+yanmartins@users.noreply.github.com>
2018-09-11 17:05:13 -0300

    Alterações nas funções contidas em vira()
    Alteração nas funções detectarObstaculos(), esquerda() e direita(), criação das funções re(), frente().

--- Movimento.py
+++ Movimento.py
@@ -5,16 +5,15 @@ from time import sleep
 from ev3dev.ev3 import *
 
 # ------Input--------
-power = 30		#Potência do motor.
+power = 40			#Potência do motor.
 target = 55
 kp = float(0.65) 	# Proportional gain. Start value 1.
 kd = 1           	# Derivative gain. Start value 0.
 ki = float(0.02) 	# Integral gain. Start value 0.
 direction = -1 		# Define a borda da linha que o sensor de cor seguirá.
-minRef = 41		# Valor mínimo refletido.
-maxRef = 63		# Valor máximo refletido.
+minRef = 41			# Valor mínimo refletido.
+maxRef = 63			# Valor máximo refletido.
 # -------------------
-
 
 # Conecta os dois LargeMotor às portas B e C.
 left_motor = LargeMotor(OUTPUT_C)
@@ -32,38 +31,102 @@ sensorUS.mode='US-DIST-CM'
 
 def vira():
 	direcao = input("Informe a direcao: ")
-	print(direcao)
 	
 	cor = sensorCor.value()
-	if direcao == '1':
-		print("Virando para a esquerda")
-		while (cor != 1) and (cor != 6): # Enquanto for verde
-			left_motor.stop(stop_action='brake')
-			right_motor.run_forever(speed_sp=100)
-			cor = sensorCor.value()
+	if direcao == '0':
+		frente(cor)
+		pass
+	
+	elif direcao == '1':
+		esquerda(cor)
 		pass
 		
 	elif direcao == '2':
-		print("Virando para a direita")
-		
-		while (cor != 1) and (cor != 6): # Enquanto for verde 
-			right_motor.run_forever(speed_sp=-50)
-			left_motor.run_forever(speed_sp=100) # encontra preto, branco e outro preto (Verificar)
-			cor = sensorCor.value()
-		right_motor.run_forever(speed_sp=0)
-		left_motor.run_forever(speed_sp=100)
-		sleep(0.8)
+		direita(cor)
 		pass
+	
+	elif direcao == '3':
+		re(cor)
+		pass
+	
 	seguirLinha(power, target, kp, kd, ki, direction, minRef, maxRef)
 
-def paradaDeEmergencia():
-	distancia = sensorUS.value()/10 
-	while distancia < 10: # Enquanto a distância for menor que 10 cm
-		right_motor.stop(stop_action='brake')
-		left_motor.stop(stop_action='brake')
-		distancia = sensorUS.value()/10
 
-def guiarPelaLinha(course, power):
+
+def frente(cor):
+	print("Indo para a frente")
+	while (cor != 1) and (cor != 6): # Enquanto for verde
+		cor = sensorCor.value()
+		left_motor.run_forever(speed_sp=100)
+		right_motor.run_forever(speed_sp=50)
+
+
+
+def esquerda(cor):
+	print("Virando para a esquerda")
+	while (cor != 1) and (cor != 6): # Enquanto for verde
+		cor = sensorCor.value()
+		left_motor.stop(stop_action='brake')
+		right_motor.run_forever(speed_sp=100)
+	while (cor != 1):
+		cor = sensorCor.value()
+		left_motor.stop(stop_action='brake')
+		right_motor.run_forever(speed_sp=200)
+	
+def direita(cor):
+	#print("Virando para a direita")
+	frente(cor)
+		
+	right_motor.stop(stop_action='brake')
+	left_motor.run_forever(speed_sp=200)
+	sleep(1)
+	sleep(1)
+	
+	while (cor != 1):
+		cor = sensorCor.value()
+		right_motor.stop(stop_action='brake')
+		left_motor.run_forever(speed_sp=200)
+
+def re(cor):
+	print("Marcha a re")
+	direita(cor)
+	direita(cor)
+
+# Função incompleta - VOLTAR PARA A POSIÇÃO ANTERIOR
+def detectarObstaculos():
+	distancia = sensorUS.value()/10
+	if distancia < 10: # Se há obstáculos
+		print("Obstaculo")
+		sensorCor.mode='COL-COLOR'		# Altera para modo cor
+		cor = sensorCor.value()
+		direita(cor)
+		left_motor.run_direct()
+		right_motor.run_direct()
+
+'''
+def encontraInterseccao():
+	sensorCor.mode='COL-COLOR'		# Altera para modo cor
+	cor = sensorCor.value()
+	if (cor != 1) and (cor != 6):
+		left_motor.stop(stop_action='brake')
+		right_motor.stop(stop_action='brake')
+		return True
+	else:
+		return False
+
+'''
+
+def encontraInterseccao():
+	sensorCor.mode='COL-COLOR'		# Altera para modo cor
+	cor = sensorCor.value()
+	if (cor == 3):
+		left_motor.stop(stop_action='brake')
+		right_motor.stop(stop_action='brake')
+		return True
+	else:
+		return False
+
+def manterNaLinha(course, power):
 	power_left = power_right = power
 	s = (50 - abs(float(course))) / 50
 	if course >= 0:
@@ -76,14 +139,22 @@ def guiarPelaLinha(course, power):
 			power_left = - power
 	return (int(power_left), int(power_right))
 
+
+
 def seguirLinha(power, target, kp, kd, ki, direction, minRef, maxRef):
 
 	lastError = error = integral = 0
+	
+	interseccaoEncontrada = encontraInterseccao()
+	if interseccaoEncontrada: # Encontra a intersecção da posição incial
+		vira()
+		
 	left_motor.run_direct()
 	right_motor.run_direct()
-	while not btn.any() :
 	
-		paradaDeEmergencia() # Para quando encontra um obstáculo
+	while not btn.any() :
+		
+		detectarObstaculos() # Para quando encontra um obstáculo
 		
 		sensorCor.mode = 'COL-REFLECT'	# Altera para modo refletido
 		refRead = sensorCor.value()
@@ -92,20 +163,16 @@ def seguirLinha(power, target, kp, kd, ki, direction, minRef, maxRef):
 		lastError = error
 		integral = float(0.5) * integral + error
 		course = (kp * error + kd * derivative +ki * integral) * direction
-		for (motor, pow) in zip((left_motor, right_motor), guiarPelaLinha(course, power)):
+		for (motor, pow) in zip((left_motor, right_motor), manterNaLinha(course, power)):
 			motor.duty_cycle_sp = pow
 		sleep(0.01) # Aprox. 100Hz
 		
-		sensorCor.mode='COL-COLOR'	# Altera para modo cor
-		cor = sensorCor.value()
-		if (cor != 1) and (cor != 6):
-			left_motor.stop(stop_action='brake')
-			right_motor.stop(stop_action='brake')
+		interseccaoEncontrada = encontraInterseccao()
+		if interseccaoEncontrada:
 			vira()
 			break
-			
-while not btn.any() :
-	seguirLinha(power, target, kp, kd, ki, direction, minRef, maxRef)
+
+seguirLinha(power, target, kp, kd, ki, direction, minRef, maxRef)
 
 # Para os motores após sair do looping.
 left_motor.stop(stop_action='brake')
