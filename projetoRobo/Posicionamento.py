90a407bb59f623a3ae29eb606a806b5b650835f5
yanmartins <30724634+yanmartins@users.noreply.github.com>
2018-09-12 14:20:25 -0300

    Alterando classe Posicionamento

--- projetoRobo/Posicionamento.py
+++ projetoRobo/Posicionamento.py
@@ -1,7 +1,24 @@
 class Posicionamento(object):
 	
-	
 	def __init__(x, y, ori):
 		eixoX = x
 		eixoY = y
+		orientacao = ori
+	
+	def getEixoX():
+		return eixoX
+	
+	def getEixoY():
+		return eixoY
+	
+	def getOrientacao():
+		return orientacao
+	
+	def setEixoX(x):
+		eixoX = x
+	
+	def setEixoY(y):
+		eixoY = y
+	
+	def setOrientacao(ori):
 		orientacao = ori
