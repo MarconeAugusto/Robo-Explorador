041eced818e605d9d28c82b574f244500c04c96a
Ameliza <36745160+Ameliza@users.noreply.github.com>
2018-09-12 19:33:18 -0300

    Atualizando a classe Manual
    Alterando o nome da função vira para move

--- projetoRobo/Manual.py
+++ projetoRobo/Manual.py
@@ -3,7 +3,7 @@ from projetoRobo import Movimento
 class Manual:
 	
 	def setAtualizaPosicao(direcao): #utilizado para informar qual movimento o robô deve fazer
-		return Movimento.vira(direcao) # alterar o nome vira para move
+		return Movimento.move(direcao)
 		
 	def getPosicaoAtual():#utilizado para validar caças
 		return Movimento.getPosicaoAtual()
