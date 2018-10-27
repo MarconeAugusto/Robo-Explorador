d86522bf4388f1f704e898e0980d81f9bbebe4f1
yanmartins <30724634+yanmartins@users.noreply.github.com>
2018-10-27 18:08:37 -0300

    Delete Manual.py

--- projetoRobo/Manual.py
+++
@@ -1,9 +0,0 @@
-from projetoRobo import Movimento
-
-class Manual:
-	
-	def setAtualizaPosicao(direcao): #utilizado para informar qual movimento o robô deve fazer
-		return Movimento.move(direcao)
-		
-	def getPosicaoAtual():#utilizado para validar caças
-		return Movimento.getPosicaoAtual()
