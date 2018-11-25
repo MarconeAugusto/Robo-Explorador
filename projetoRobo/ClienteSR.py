856efc4960b26e2ae62f971ddfee40cb8009c6a6
yanmartins <30724634+yanmartins@users.noreply.github.com>
2018-11-24 23:10:29 -0200

    Adequando classe para uso do SA

--- projetoRobo/ClienteSR.py
+++ projetoRobo/ClienteSR.py
@@ -2,23 +2,22 @@
 # coding: utf-8
 import Pyro4
 import Pyro4.util
+from SS import *
 
 class ClienteSR:
-    def setCorLed(self):
+    def setCorLed(self, corLED):
         ns = Pyro4.locateNS("10.42.0.79")  # IP do robô
         uri = ns.lookup('ServidorSR')
         print(uri)
         o = Pyro4.Proxy(uri)
-        c = input("Informe a cor do led: ")
-        cor = int(c)
-        print(o.setCorLed(cor))
+        return o.setCorLed(corLED)
 
     def getEndMAC(self):
         ns = Pyro4.locateNS("10.42.0.79")  # IP do robô
         uri = ns.lookup('ServidorSR')
         print(uri)
         o = Pyro4.Proxy(uri)
-        print(o.getEndMAC())
+        return o.getEndMAC()
 
     ##acessada via interface gráfica
     def MovimentoManual(self, direcao):
@@ -27,13 +26,16 @@ class ClienteSR:
         print(uri)
         try:
             o = Pyro4.Proxy(uri)
-            o.move(direcao)
+            if PrincipalSS.getEstadoDoJogo(self):
+                o.move(direcao)
+            else:
+                print("JOGO PAUSADO!")
         except Exception:
             print("Pyro traceback:")
             print("".join(Pyro4.util.getPyroTraceback()))
 
     ##acessada via interface gráfica
-    def MovimentoAutonomo(self,):
+    def MovimentoAutonomo(self):
         print("Iniciando movimento Autônomo")
         ns = Pyro4.locateNS("10.42.0.79")  # IP do robô
         uri = ns.lookup('Movimento')
