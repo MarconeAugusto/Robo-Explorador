b3ced789ec1c4a294e032e5341c0ac73beb07ec5
yanmartins <30724634+yanmartins@users.noreply.github.com>
2018-11-24 23:45:20 -0200

    Adequando classe para uso do SA

--- projetoRobo/ServidorSR.py
+++ projetoRobo/ServidorSR.py
@@ -6,11 +6,23 @@ import time
 from ev3dev.ev3 import *
 import socket
 import Pyro4
+from Posicionamento import *
 
 # SERVIDOR DO SR
 
 @Pyro4.expose
 class ServidorSR:
+
+    def atualizaPosicao(self, pos, id):
+        print("Atualiza pos no SS")
+        ns = Pyro4.locateNS("191.36.15.99")  # IP do SS
+        uri = ns.lookup('listaCacas')
+        self.o = Pyro4.Proxy(uri)
+
+        x = pos.getEixoX()
+        y = pos.getEixoY()
+        ori = pos.getOrientacao()
+        self.o.atualizaPosicaoSS(x,y,ori, id)
 
     def getEndMAC(self):
         mac = ""
@@ -19,15 +31,20 @@ class ServidorSR:
         f.close()
         return mac
 
-
     def setCorLed(self, cor):
-        if cor == 1:
+        if cor == '1':
             Leds.set_color(Leds.LEFT, Leds.YELLOW)
             Leds.set_color(Leds.RIGHT, Leds.YELLOW)
-        elif cor == 2:
+            return 1
+        elif cor == '2':
             Leds.set_color(Leds.LEFT, Leds.RED)
             Leds.set_color(Leds.RIGHT, Leds.RED)
-        else:
+            return 2
+        elif cor == '3':
             Leds.set_color(Leds.LEFT, Leds.GREEN)
             Leds.set_color(Leds.RIGHT, Leds.GREEN)
-        return "Teste"
+            return 3
+        elif cor == '4':
+            Leds.set_color(Leds.LEFT, Leds.BLACK)
+            Leds.set_color(Leds.RIGHT, Leds.BLACK)
+            return 4
